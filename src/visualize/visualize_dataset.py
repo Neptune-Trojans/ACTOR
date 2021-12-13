3c5e68065174a28db4348607e26e73647e283f4e
Paul.Yudkin <paul.yudkin@datagen.tech>
2021-12-13 14:23:49 +0200

    added visualization stats

--- src/visualize/visualize_dataset.py
+++ src/visualize/visualize_dataset.py
@@ -1,15 +1,21 @@
 import matplotlib.pyplot as plt
-# import torch
 import os
+import pandas as pd
+import plotly.express as px
 
 from src.datasets.get_dataset import get_dataset
 from src.parser.visualize import parser
-# from src.utils import optutils
-
-import src.utils.fixseed  # noqa
 from src.visualize.visualize import viz_dataset
 
 plt.switch_backend('agg')
+
+
+def build_dataset_dist(dataset):
+    frames_by_class = zip(dataset._actions, dataset._num_frames_in_video)
+    frames_by_class_df = pd.DataFrame(frames_by_class, columns=['action', 'frames'])
+    frames_by_class_df.replace({"action":  dataset._action_classes}, inplace=True)
+    fig = px.histogram(frames_by_class_df, x="frames", color="action", barmode='overlay', title='frames by action')
+    fig.write_html(os.path.join("datavisualize", 'HumanAct12_frames_by_action.html'))
 
 
 if __name__ == '__main__':
@@ -24,7 +30,7 @@ if __name__ == '__main__':
     # get data
     DATA = get_dataset(name=parameters["dataset"])
     dataset = DATA(split="train", **parameters)
-
+    build_dataset_dist(dataset)
     # add specific parameters from the dataset loading
     dataset.update_parameters(parameters)
 
