81ec07596a375f61d3a508a131b9bfda68d98a4d
Paul.Yudkin <paul.yudkin@datagen.tech>
2022-02-13 15:18:07 +0200

    correct amout of joints

--- src/datasets/datagen.py
+++ src/datasets/datagen.py
@@ -37,7 +37,7 @@ class Datagen(Dataset):
         return self._joints[ind][frame_ix]
 
     def _load_rotvec(self, ind, frame_ix):
-        pose = self._pose[ind][frame_ix].reshape(-1, 24, 3)
+        pose = self._pose[ind][frame_ix].reshape(-1, 62, 3)
         return pose
 
 
