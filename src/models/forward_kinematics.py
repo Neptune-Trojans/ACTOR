30fa812a6669e3e8e71f26f7389dd1b7bb488d47
Paul.Yudkin <paul.yudkin@datagen.tech>
2022-02-20 09:07:48 +0200

    added new files

---
+++ src/models/forward_kinematics.py
@@ -0,0 +1,84 @@
+import numpy as np
+import torch
+from pytorch3d.transforms import quaternion_multiply, quaternion_apply
+
+
+class Skeleton:
+    def __init__(self, offsets, parents, joints_left=None, joints_right=None):
+        assert len(offsets) == len(parents)
+
+        self._offsets = torch.FloatTensor(offsets)
+        self._parents = np.array(parents)
+        self._joints_left = joints_left
+        self._joints_right = joints_right
+        self._compute_metadata()
+
+    def cuda(self):
+        self._offsets = self._offsets.cuda()
+        return self
+
+    def num_joints(self):
+        return self._offsets.shape[0]
+
+    def offsets(self):
+        return self._offsets
+
+    def parents(self):
+        return self._parents
+
+    def has_children(self):
+        return self._has_children
+
+    def children(self):
+        return self._children
+
+    def forward_kinematics(self, rotations, root_positions):
+        """
+        Perform forward kinematics using the given trajectory and local rotations.
+        Arguments (where N = batch size, L = sequence length, J = number of joints):
+         -- rotations: (N, L, J, 4) tensor of unit quaternions describing the local rotations of each joint.
+         -- root_positions: (N, L, 3) tensor describing the root joint positions.
+        """
+        assert len(rotations.shape) == 4
+        assert rotations.shape[-1] == 4
+
+        positions_world = []
+        rotations_world = []
+
+        expanded_offsets = self._offsets.expand(rotations.shape[0], rotations.shape[1],
+                                                self._offsets.shape[0], self._offsets.shape[1])
+
+        # Parallelize along the batch and time dimensions
+        for i in range(self._offsets.shape[0]):
+            if self._parents[i] == -1:
+                positions_world.append(root_positions)
+                rotations_world.append(rotations[:, :, 0])
+            else:
+                positions_world.append(quaternion_apply(rotations_world[self._parents[i]], expanded_offsets[:, :, i]) \
+                                       + positions_world[self._parents[i]])
+                if self._has_children[i]:
+                    rotations_world.append(quaternion_multiply(rotations_world[self._parents[i]], rotations[:, :, i]))
+                else:
+                    # This joint is a terminal node -> it would be useless to compute the transformation
+                    rotations_world.append(None)
+
+        return torch.stack(positions_world, dim=3).permute(0, 1, 3, 2)
+
+    def joints_left(self):
+        return self._joints_left
+
+    def joints_right(self):
+        return self._joints_right
+
+    def _compute_metadata(self):
+        self._has_children = np.zeros(len(self._parents)).astype(bool)
+        for i, parent in enumerate(self._parents):
+            if parent != -1:
+                self._has_children[parent] = True
+
+        self._children = []
+        for i, parent in enumerate(self._parents):
+            self._children.append([])
+        for i, parent in enumerate(self._parents):
+            if parent != -1:
+                self._children[parent].append(i)
---
+++ src/utils/bvh_export.py
@@ -0,0 +1,26 @@
+import os.path
+
+from fairmotion.core.motion import Motion
+from fairmotion.data import bvh
+from fairmotion.ops.conversions import R2T
+
+from src.datasets.default_skeleton import DefaultSkeleton
+import src.utils.rotation_conversions as geometry
+
+
+def save_generated_motion(generation, mask, save_path, params):
+    if not ((params['dataset'] == 'datagen') & (params['pose_rep'] == 'rot6d')):
+        return
+    skl = DefaultSkeleton('src/datasets/skeleton.pkl')
+    x_translations = generation[:, -1, :3]
+    x_rotations = generation[:, :-1]
+
+    x_rotations = x_rotations.permute(0, 3, 1, 2)
+    nsamples, time, njoints, feats = x_rotations.shape
+    for i in range(nsamples):
+        rotations = geometry.rotation_6d_to_matrix(x_rotations[i][mask[i]])
+        rotations = rotations.clone().detach().cpu().numpy()
+        new_motion = Motion.from_matrix(R2T(rotations), skl.skeleton)
+        bvh.save(new_motion, os.path.join(save_path, f'gen_motion_{i}.bvh'))
+
+
