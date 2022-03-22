6fe0308c9ed7d5ea9df9a1cdd7e947bad4076e9d
Paul.Yudkin <paul.yudkin@datagen.tech>
2022-03-22 13:52:37 +0200

    changes

--- src/visualize/visualize_checkpoint.py
+++ src/visualize/visualize_checkpoint.py
@@ -6,7 +6,7 @@ from src.utils.get_model_and_data import get_model_and_data
 from src.parser.visualize import parser
 from visualize import viz_epoch
 
-import src.utils.fixseed  # noqa
+# import src.utils.fixseed  # noqa
 
 plt.switch_backend('agg')
 
@@ -14,12 +14,19 @@ plt.switch_backend('agg')
 def main():
     # parse options
     parameters, folder, checkpointname, epoch = parser()
-    parameters['pose_rep'] = 'xyz'
-    parameters['num_frames'] = 120
+    parameters['pose_rep'] = 'rot6d'
+    parameters['num_frames'] = 200
+
+    parameters['decoder_test'] = "new"
+    parameters["noise_same_action"] = 'random'
+    parameters['fps'] = 10
+
+    parameters['dataset'] = 'datagen'
+    parameters['jointstype'] = 'datagen_skeleton'
+    parameters["num_actions_to_sample"] = 1
+
     model, datasets = get_model_and_data(parameters)
     dataset = datasets["train"]
-    parameters['decoder_test'] = "diffduration"
-
     print("Restore weights..")
     checkpointpath = os.path.join(folder, checkpointname)
     state_dict = torch.load(checkpointpath, map_location=parameters["device"])
